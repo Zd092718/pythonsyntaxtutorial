b8cb1d5858d3db83fb559f4bddead4a622306f3d
Zachary Dowd <zacharyd2796@zohomail.com>
2021-10-27 17:52:06 -0600

    function notes added

--- helloworld.py
+++ helloworld.py
@@ -1,4 +1,7 @@
+# print to console
 print("Hello world")
+
+# variable declarations/ nice and easy
 age = 15
 
 # %d is an integer placeholder %s is a string placeholder
@@ -10,3 +13,35 @@ elif(age == 25):
 else:
     sent = "You are %d years old wee child"
     print(sent % age)
+
+# function defs
+
+
+def HelloWorld():
+    print("Hello World")
+
+
+HelloWorld()
+
+
+def Greeting(name):
+    print("Hi " + name + "!")
+
+
+Greeting("Zack")
+
+
+def Sum(num1, num2):
+    print(num1 + num2)
+
+
+Sum(5, 40)
+
+
+def returnSum(num1, num2):
+    return (num1 + num2)
+
+
+sum = returnSum(12, 23)
+
+print(sum)
